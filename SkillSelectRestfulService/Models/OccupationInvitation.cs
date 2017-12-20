5dc8c9bb950746c21070c51fe80e9719dd157d4a
Jonathan Zhao <jzjonathanzz@gmail.com>
2017-12-20 16:33:05 +1100

    XML Data file and Model Template Created.

---
+++ SkillSelectRestfulService/Models/OccupationInvitation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillSelectRestfulService.Models
+{
+    public class OccupationInvitation
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Ceiling { get; set; }
+
+        public int Invitations { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime VisaDateofEffect { get; set; }
+    }
+}
